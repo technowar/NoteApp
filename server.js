3ff423ec32a8395ac64b9b4df6022f7279a246db
J4hmTor <johmel.rey.m.pintor@accenture.com>
2017-08-09 22:00:36 +0800

    Mongoose commit

--- server.js
+++ server.js
@@ -1,17 +1,43 @@
 var express = require('express');
+var mongoose = require('mongoose');
 
 var expressConfig = require('./app/config/ExpressConfig');
 var routeConfig = require('./app/config/RouteConfig');
+var config = require('./app/config.json');
 
 var App = express();
 
 expressConfig(App);
 routeConfig(App);
 
-App.listen(3000, function(error) {
-  if (error) {
-    throw error;
-  }
+connect()
+  .on('error', console.log)
+  .on('disconnected', connect)
+  .once('open', listen);
 
-  console.log('Starting server');
-});
+function listen() {
+  App.listen(3000, function(error) {
+    if (error) {
+      throw error;
+    }
+
+    console.log('Starting server');
+  });
+}
+
+function connect() {
+  var database = [
+    'mongodb://',
+    config.db.username,
+    ':',
+    config.db.password,
+    '@ds133438.mlab.com:33438/',
+    config.db.database
+  ].join('');
+
+  return mongoose.connect(database, {
+    socketTimeoutMS: 30000,
+    keepAlive: true,
+    reconnectTries: 30
+  }).connection;
+}
