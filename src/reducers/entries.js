455154cfd8673792226bb8ed58ce18ab528a5775
J4hmTor <johmel.rey.m.pintor@accenture.com>
2017-08-14 18:25:15 +0800

    Insert entry in top of the list

--- src/reducers/entries.js
+++ src/reducers/entries.js
@@ -6,9 +6,9 @@ import {
 const entry = (state = [], action) => {
   switch (action.type) {
     case ADD_ENTRY_SUCCESS:
-      return [...state, JSON.parse(action.response)]
+      return [JSON.parse(action.response), ...state]
     case GET_ENTRIES_SUCCESS:
-      return JSON.parse(action.response)
+      return JSON.parse(action.response).reverse()
     default:
       return state
   }
